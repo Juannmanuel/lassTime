31012b899e1e1deb1514549672b9b3708d6f52e1
Juan Manuel <losredondos551@gmail.com>
2023-12-08 05:06:51 -0300

    foooter

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,16 @@ import style from "./Footer.module.css"
 const Footer = () => {
  return (
     <div>
-        <h1>Footer</h1>
+         <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">
+        &copy; 2023 Juan Manuel.
+      </small>
+      <p className="text-xs">
+        <span className="font-semibold">Acerca de este sitio web:</span> construido con
+        React y Next.js (App Router y Server Actions), TypeScript, Tailwind CSS,
+        Framer Motion, React Email & Resend, alojado en Vercel.
+      </p>
+    </footer>
     </div>
  )   
 }
